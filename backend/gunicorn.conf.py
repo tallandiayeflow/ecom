deb0a5405b9b1dd96cba6b5e533f4dcf7f54a500
Talla ndiaye <ndiayetalla928@gmail.com>
2025-11-14 00:12:53 +0000

    Test déploiement automatique

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -7,13 +7,13 @@ timeout = 300
 # Workers basés sur CPU
 workers = multiprocessing.cpu_count() * 2 + 1
 
-# Bind sur le port Render
+# Bind sur le port Render ddd
 bind = f"0.0.0.0:{os.getenv('PORT', '5000')}"
 
 # Classe de workers
 worker_class = "sync"
 
-# Max requests pour éviter les fuites mémoire
+# Max requests pour éviter les fuites mémoire    
 max_requests = 1000
 max_requests_jitter = 50
 
